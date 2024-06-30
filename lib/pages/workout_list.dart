ddaf4638622788bfee34dc046f7e6f0b8a8ece2a
omarabdul <omaraabdul9@gmail.com>
2024-06-29 20:25:46 -0400

    can now select workout templates

--- lib/pages/workout_list.dart
+++ lib/pages/workout_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '/models/workout.dart';
 import '/services/workout_service.dart';
+import '/pages/workout_detail_page.dart';
 
 class WorkoutList extends StatelessWidget {
   @override
@@ -13,13 +14,21 @@ class WorkoutList extends StatelessWidget {
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
+          return Center(child: CircularProgressIndicator());
         }
 
         return ListView(
           children: snapshot.data!.map((workout) => ListTile(
             title: Text(workout.name),
             subtitle: Text('${workout.exercises.length} exercises'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => WorkoutDetailPage(workout: workout),
+                ),
+              );
+            },
           )).toList(),
         );
       },
